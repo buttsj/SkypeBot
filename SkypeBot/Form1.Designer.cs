db5623792ed9964ec18889f8440b89a17f766640
John (Jack) Butts <butts.102@osu.edu>
2015-10-21 11:22:56 -0400

    Initial commit

---
+++ SkypeBot/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace SkypeBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sendMassMessage = new System.Windows.Forms.Button();
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // sendMassMessage
+            // 
+            this.sendMassMessage.Location = new System.Drawing.Point(12, 226);
+            this.sendMassMessage.Name = "sendMassMessage";
+            this.sendMassMessage.Size = new System.Drawing.Size(260, 23);
+            this.sendMassMessage.TabIndex = 0;
+            this.sendMassMessage.Text = "Send Message";
+            this.sendMassMessage.UseVisualStyleBackColor = true;
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.Location = new System.Drawing.Point(11, 12);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(261, 121);
+            this.lstBox.TabIndex = 1;
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(12, 137);
+            this.txtBox.Multiline = true;
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(260, 83);
+            this.txtBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.lstBox);
+            this.Controls.Add(this.sendMassMessage);
+            this.Name = "Form1";
+            this.Text = "Skype Bot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sendMassMessage;
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.TextBox txtBox;
+    }
+}
+
---
+++ SkypeBot/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using SKYPE4COMLib;
+using System.Windows.Forms;
+
+namespace SkypeBot
+{
+    public partial class Form1 : Form
+    {
+        Skype skype;
+        String trigger = "!";
+        String nickname = "//JACKBOT\\\\ ";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            skype = new Skype();
+            skype.Attach(7, false);
+            skype.MessageStatus += new _ISkypeEvents_MessageStatusEventHandler(skype_MessageStatus);
+        }
+
+        private void skype_MessageStatus(ChatMessage pMessage, TChatMessageStatus Status)
+        {
+            if (Status == TChatMessageStatus.cmsReceived || Status == TChatMessageStatus.cmsSent)
+            {
+                String msg = pMessage.Body;
+                Chat c = pMessage.Chat;
+
+                /*if (pMessage.Sender.Handle == "eeshwan")
+                {
+                    c.SendMessage(nickname + "Memer detected...");
+                } else if (pMessage.Sender.Handle == "dalton.f7")
+                {
+                    c.SendMessage(nickname + "ooh a message from dalton-sama");
+                } else if (pMessage.Sender.Handle == "dream_3ater")
+                {
+                    c.SendMessage(nickname + "the creator...");
+                }*/
+
+                if (msg.StartsWith(trigger))
+                {
+                    lstBox.Items.Add(DateTime.Now.ToLongTimeString() + ":" + "command " + "'" + msg + "'" + " from " + pMessage.Sender.Handle);
+                    msg = msg.Remove(0, 1).ToLower();
+
+                    if (msg == "jackbot")
+                    {
+                        c.SendMessage(nickname + "Hello my good friend. Hope you have a jooday.");
+                    }else if (msg == "meme")
+                    {
+                        c.SendMessage(nickname + "Meme I'm enjoying lately: https://www.youtube.com/watch?v=fK1N_vqJPac");
+                    }else if (msg == "dnd")
+                    {
+                        c.SendMessage(nickname + "D&D is scheduled for Saturday October 24th, sometime around 11pm.");
+                    } else
+                    {
+                        c.SendMessage(nickname + "I'm not sure what you want from me...");
+                    }
+                }
+            }
+        }
+    }
+}
